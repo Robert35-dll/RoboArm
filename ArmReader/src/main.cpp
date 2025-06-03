96a4717fab6efef582900179d24a409d31f15da7
Robert Koch <Robert.Koch@Student.HTW-Berlin.de>
2025-06-03 22:05:50 +0200

    Arm reader setup (#1)
    * Initialized basic joystick reading with serial communication which will be replaced with BLE
    
    * Replaced the serial with radio communication via RF24 module
    
    * Added vs-code workspace file
    
    * Removed casting joystick's input from int to short

--- ArmReader/src/main.cpp
+++ ArmReader/src/main.cpp
@@ -1,9 +1,77 @@
 #include <Arduino.h>
 
-void setup() {
+// Radio libraries
+#include <SPI.h>
+#include <RF24.h>
 
+#define CE_PIN 7
+#define CSN_PIN 8
+
+RF24 Transmitter(CE_PIN, CSN_PIN);
+const byte T_ADDRESS[6] = "1RF24";
+
+// The joystick's vertical axis' pin
+#define J_PIN_Y A0
+// The joystick's toggle button pin
+#define J_PIN_B A1
+
+uint16_t JYAxisInput;
+bool ButtonPressed;
+
+// The data transfer array
+byte JTransfer[2];
+
+void PrintData();
+
+void setup() {
+    Serial.begin(9600);
+
+    // Joystick setup
+    pinMode(J_PIN_Y, INPUT);
+    pinMode(J_PIN_B, INPUT);
+
+    // Transmitter setup
+    if (!Transmitter.begin()) {
+        Serial.println("[!]-< Transmitter failed T_T");
+        while (1);
+    }
+    Serial.println("[*]-< Transmitter started ^~^");
+
+    // Setting data channel
+    Transmitter.setDataRate(RF24_250KBPS);
+    Transmitter.setPALevel(RF24_PA_LOW);
+    Transmitter.setChannel(88);
+    // Setting transmission's properties
+    Transmitter.setPayloadSize(sizeof(JTransfer));
+    Transmitter.setAutoAck(true);
+    // Setting an amount and delay to retry failed transmissions
+    Transmitter.setRetries(5, 3);
+    // Setting the transmitter to send data
+    Transmitter.openWritingPipe(T_ADDRESS);
+    Transmitter.stopListening();
+
+    Serial.println("[AR]-< Initialized");
 }
 
 void loop() {
+    JYAxisInput = analogRead(J_PIN_Y);
+    ButtonPressed = analogRead(J_PIN_B) <= 100;
 
+    JTransfer[0] = JYAxisInput;
+    JTransfer[1] = ButtonPressed;
+
+    // Writing one (first) byte to the radio channel
+    if (Transmitter.write(&JTransfer, sizeof(JTransfer), 0)) {
+        PrintData();
+    }
+
+    delay(1000);
+}
+
+void PrintData() {
+    Serial.print("[+]-< Joystick Y-axis input: ");
+    Serial.println(JYAxisInput);
+    Serial.print("[+]-< Joystick button signal: ");
+    Serial.println(ButtonPressed);
+    Serial.println(" |");
 }
