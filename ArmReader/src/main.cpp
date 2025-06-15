0d32376f65109371baf9c96a571682c5e33e6bae
Robert Koch <Robert.Koch@Student.HTW-Berlin.de>
2025-06-15 22:34:27 +0200

    Refactored ArmReader to process MPUs manually since no library can work with two modules simultaneously -_-

--- ArmReader/src/main.cpp
+++ ArmReader/src/main.cpp
@@ -1,3 +1,9 @@
+// * Note: This code is created out of pure blood and sweat of its author.
+// * The basic functionality is identical to any other MPU6050 library.
+// * I used Copilot to understand and to extend their code though :).
+// * Here's the conversation that provided me the basic knowledge:
+// * https://copilot.microsoft.com/shares/87RBiVezU2dXnzmMsvEa3
+
 #include <Arduino.h>
 
 // Radio libraries
@@ -10,26 +16,43 @@
 RF24 Transmitter(CE_PIN, CSN_PIN);
 const byte T_ADDRESS[6] = "1RF24";
 
-// MPU6050's libraries
-#include <MPU6050_tockn.h>
+// MPU6050s' preparation
 #include <Wire.h>
 
-// The MPUs' mute pins
-#define UPPER_MPU_ADO 2
-#define LOWER_MPU_ADO 3
+// The MPUs' addresses to read I2C data from 
+#define LOWER_MPU_ADR 0x68                  // This is standard address
+#define UPPER_MPU_ADR 0x69                  // and this is modified
 
-const int AnglesAmount = 3;
-// Arrays for MPUs' values
-int UpperMPUAngles[AnglesAmount];
-int LowerMPUAngles[AnglesAmount];
+const int Addresses[2] = {LOWER_MPU_ADR, UPPER_MPU_ADR};
 
-// The MPU module Arduino's currently listening to
-MPU6050 ActiveMPU(Wire);
+// The MPUs' ADO pins to tune the addresses
+#define LOWER_MPU_ADO 2                     // This shouldn't be powered -> ADR = 0x68
+#define UPPER_MPU_ADO 3                     // This should be powered -> ADR = 0x69
+
+const int MPUsAmount = 2;
+const int AxisAmount = 3;
+// Arrays for accelerometers' and gyroscopes' values
+float Accs[MPUsAmount][AxisAmount];
+float Rots[MPUsAmount][AxisAmount];
+// Array for acceleration's angles
+float AccAngs[MPUsAmount][AxisAmount];
+
+// Arrays for ouput angles
+float UpperAngles[AxisAmount];  // The angles of upper arm
+float LowerAngles[AxisAmount];  // The angles of underarm
+
+// Correction variables
+float AccMagnitude;                         // To determine whether MPU is moving or not
+float Alphas[MPUsAmount];                   // To adjust acc/gyro influence upon movements
+float GyroCorrs[MPUsAmount][AxisAmount];    // To allow fast rotations (high-pass)
+float GyroBiases[MPUsAmount][AxisAmount];   // To reduce small drifts (low-pass)
+
+#define TIMEOUT 10
 
 // The joystick's vertical axis' pin
-#define J_PIN_Y A0
+#define J_PIN_Y A1
 // The joystick's toggle button pin
-#define J_PIN_B A1
+#define J_PIN_B A0
 
 uint16_t JYAxisInput;
 bool ButtonPressed;
@@ -37,7 +60,12 @@ bool ButtonPressed;
 // The data transfer array
 byte JTransfer[2];
 
-void GetAngles(int, int , int (*)[3]);
+void InitMPU(int);
+
+void GetAngles(int, float[3], bool);
+void CorrectAngles(int, float[3]);
+void GetRawAccelerations(int);
+void GetRawRotations(int);
 void PrintData();
 
 void setup() {
@@ -50,11 +78,14 @@ void setup() {
     digitalWrite(UPPER_MPU_ADO, HIGH);
     digitalWrite(LOWER_MPU_ADO, LOW);
 
-    // Initializing and calibrating the active MPU module 
-    Serial.println("[*]-< Initializing MPU monitoring object");
+    // Initializing and calibrating the active MPU module
     Wire.begin();
-    ActiveMPU.begin();
-    ActiveMPU.calcGyroOffsets();
+
+    Serial.println("[*]-< Initializing LowerMPU monitoring object");
+    InitMPU(LOWER_MPU_ADR);
+
+    Serial.println("[*]-< Initializing UpperMPU monitoring object");
+    InitMPU(UPPER_MPU_ADR);
 
     // Joystick setup
     pinMode(J_PIN_Y, INPUT);
@@ -85,8 +116,9 @@ void setup() {
 
 void loop() {
     // Reading angles of the arm
-    GetAngles(LOWER_MPU_ADO, UPPER_MPU_ADO, &UpperMPUAngles);
-    GetAngles(UPPER_MPU_ADO, LOWER_MPU_ADO, &LowerMPUAngles);
+    GetAngles(0, LowerAngles, true);
+    GetAngles(1, UpperAngles, true);
+
     // TODO: Packing them into the transfer array
 
     JYAxisInput = analogRead(J_PIN_Y);
@@ -95,61 +127,112 @@ void loop() {
     JTransfer[0] = JYAxisInput;
     JTransfer[1] = ButtonPressed;
 
+    PrintData();
     // Writing one (first) byte to the radio channel
-    if (Transmitter.write(&JTransfer, sizeof(JTransfer), 0)) {
-        PrintData();
-    }
+    // if (Transmitter.write(&JTransfer, sizeof(JTransfer), 0)) {
+    // }
 
-    delay(1000);
+    delay(TIMEOUT);
 }
 
-void GetAngles(int mutePin, int unMutePin, int (*angleArrPtr)[3]) {
-    // Muting the given (other) MPU module
-    digitalWrite(mutePin, HIGH);
-    digitalWrite(unMutePin, LOW);
+void InitMPU(int adr) {
+    Wire.beginTransmission(adr);
+    // Talk to the register 6B and reset it by writing a 0
+    Wire.write(0x6B);
+    Wire.write(0x00);
+    // End the transmission with a stop message
+    Wire.endTransmission(true);
+}
 
-    delay(1000);
+/// @brief Calculates rotation angles of a certain MPU module.
+/// @param mpuNum - The number of an MPU module (either 0 or 1).
+/// @param outArr - The output array to save calculations into.
+/// @param corrReq - The flag to determine,
+/// whether the angles should be automatically corrected or not.
+void GetAngles(int mpuNum, float (outArr)[3], bool corrReq) {
+    // Reading MPU's raw data
+    GetRawAccelerations(mpuNum);
+    GetRawRotations(mpuNum);
 
-    // Updating the data of currently active one
-    ActiveMPU.update();
+    // Calculating angles in degrees (°)
+    AccAngs[mpuNum][0] = atan(Accs[mpuNum][1] / sqrt(pow(Accs[mpuNum][0], 2) + pow(Accs[mpuNum][2], 2))) * 180 / PI;
+    AccAngs[mpuNum][1] = atan(-(Accs[mpuNum][0]) / sqrt(pow(Accs[mpuNum][1], 2) + pow(Accs[mpuNum][2], 2))) * 180 / PI;
+    // * Note: the Z-axis doesn't have any gravity reference, so its calculation can be omitted :)
+    AccAngs[mpuNum][2] = atan((Accs[mpuNum][2]) / sqrt(pow(Accs[mpuNum][0], 2) + pow(Accs[mpuNum][1], 2))) * 180 / PI;
 
-    int x = ActiveMPU.getGyroAngleX();
-    int y = ActiveMPU.getGyroAngleY();
-    int z = ActiveMPU.getGyroAngleZ();
+    if (corrReq) {
+        CorrectAngles(mpuNum, outArr);
+    } else {
+        outArr[0] = AccAngs[mpuNum][0];
+        outArr[1] = AccAngs[mpuNum][1];
+        outArr[2] = AccAngs[mpuNum][2];
+    }
+}
+void CorrectAngles(int mpuNum, float outArr[3]) {
+    // Adjusting acc/gyro influence
+    AccMagnitude = sqrt(pow(AccAngs[mpuNum][0], 2) + pow(AccAngs[mpuNum][1], 2) + pow(AccAngs[mpuNum][2], 2));
+    // * Note: the accelerator has higher influence
+    // * since the arm is considered to remain static most of the time
+    Alphas[mpuNum] = abs(AccMagnitude - 1.0) > 0.1 ? 0.02 : 0.045;
+    
+    // Calculating high-passes
+    GyroCorrs[mpuNum][0] = (outArr[0] - AccAngs[mpuNum][0]) * 0.8;
+    GyroCorrs[mpuNum][1] = (outArr[1] - AccAngs[mpuNum][1]) * 0.8;
+    GyroCorrs[mpuNum][2] = (outArr[2] - AccAngs[mpuNum][2]) * 0.8;
+    // Calculating low-passes
+    GyroBiases[mpuNum][0] += Rots[mpuNum][0] * 0.0001;
+    GyroBiases[mpuNum][1] += Rots[mpuNum][1] * 0.0001;
+    GyroBiases[mpuNum][2] += Rots[mpuNum][2] * 0.0001;
 
-    (*angleArrPtr)[0] = x;
-    (*angleArrPtr)[1] = y;
-    (*angleArrPtr)[2] = z;
+    outArr[0] = Alphas[mpuNum] * (outArr[0] + (Rots[mpuNum][0] - GyroBiases[mpuNum][0]) * TIMEOUT / 1000)
+                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][0] + GyroCorrs[mpuNum][0]);
+    outArr[1] = Alphas[mpuNum] * (outArr[1] + (Rots[mpuNum][1] - GyroBiases[mpuNum][1]) * TIMEOUT / 1000)
+                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][1] + GyroCorrs[mpuNum][1]);
+    outArr[2] = Alphas[mpuNum] * (outArr[2] + (Rots[mpuNum][2] - GyroBiases[mpuNum][2]) * TIMEOUT / 1000)
+                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][2] + GyroCorrs[mpuNum][2]);
+}
 
-    Serial.print(" |-< X: ");
-    Serial.print(x);
-    Serial.print(" Y: ");
-    Serial.print(y);
-    Serial.print(" Z: ");
-    Serial.println(z);
-    Serial.println(" |");
+void GetRawAccelerations(int mpuNum) {
+    Wire.beginTransmission(Addresses[mpuNum]);
+    Wire.write(0x3B);
+    Wire.endTransmission(true);
+    Wire.requestFrom(Addresses[mpuNum], 6, true);
 
-    // Activating the muted MPU back 
-    digitalWrite(unMutePin, HIGH);
+    // The division by 16384 (depends on sensitivity) is needed
+    // to convert the sensor signals into physical values
+    Accs[mpuNum][0] = (Wire.read() << 8 | Wire.read()) / 16384.0;
+    Accs[mpuNum][1] = (Wire.read() << 8 | Wire.read()) / 16384.0;
+    Accs[mpuNum][2] = (Wire.read() << 8 | Wire.read()) / 16384.0;
+}
+void GetRawRotations(int mpuNum) {
+    Wire.beginTransmission(Addresses[mpuNum]);
+    Wire.write(0x43);
+    Wire.endTransmission(true);
+    Wire.requestFrom(Addresses[mpuNum], 6, true);
+
+    // The division by 131 (depends on sensitivity) is needed
+    // to convert the sensor signals into physical values
+    Rots[mpuNum][0] = (Wire.read() << 8 | Wire.read()) / 131.0;
+    Rots[mpuNum][1] = (Wire.read() << 8 | Wire.read()) / 131.0;
+    Rots[mpuNum][2] = (Wire.read() << 8 | Wire.read()) / 131.0;
 }
 
 void PrintData() {
     Serial.print("[+]-< Upper MPU angles: ");
     Serial.print("X: ");
-    Serial.print(UpperMPUAngles[0]);
+    Serial.print(UpperAngles[0]);
     Serial.print(" Y: ");
-    Serial.print(UpperMPUAngles[1]);
+    Serial.print(UpperAngles[1]);
     Serial.print(" Z: ");
-    Serial.println(UpperMPUAngles[2]);
-    Serial.println(" |");
+    Serial.println(UpperAngles[2]);
 
     Serial.print("[+]-< Lower MPU angles: ");
     Serial.print("X: ");
-    Serial.print(LowerMPUAngles[0]);
+    Serial.print(LowerAngles[0]);
     Serial.print(" Y: ");
-    Serial.print(LowerMPUAngles[1]);
+    Serial.print(LowerAngles[1]);
     Serial.print(" Z: ");
-    Serial.println(LowerMPUAngles[2]);
+    Serial.println(LowerAngles[2]);
     Serial.println(" |");
 
     Serial.print("[+]-< Joystick Y-axis input: ");
