4bcfd2c2d185e2b6e24528c19f0c15b43a12e050
Robert Koch <Robert.Koch@Student.HTW-Berlin.de>
2025-06-16 22:02:56 +0200

    Adjusted variables types for easier transmission via radio (needs to be tested properly)

--- ArmReader/src/main.cpp
+++ ArmReader/src/main.cpp
@@ -29,8 +29,8 @@ const int Addresses[2] = {LOWER_MPU_ADR, UPPER_MPU_ADR};
 #define LOWER_MPU_ADO 2                     // This shouldn't be powered -> ADR = 0x68
 #define UPPER_MPU_ADO 3                     // This should be powered -> ADR = 0x69
 
-const int MPUsAmount = 2;
-const int AxisAmount = 3;
+const short MPUsAmount = 2;
+const short AxisAmount = 3;
 // Arrays for accelerometers' and gyroscopes' values
 float Accs[MPUsAmount][AxisAmount];
 float Rots[MPUsAmount][AxisAmount];
@@ -38,8 +38,8 @@ float Rots[MPUsAmount][AxisAmount];
 float AccAngs[MPUsAmount][AxisAmount];
 
 // Arrays for ouput angles
-float UpperAngles[AxisAmount];  // The angles of upper arm
-float LowerAngles[AxisAmount];  // The angles of underarm
+int16_t UpperAngles[AxisAmount];                // The angles of upper arm
+int16_t LowerAngles[AxisAmount];                // The angles of underarm
 
 // Correction variables
 float AccMagnitude;                         // To determine whether MPU is moving or not
@@ -58,14 +58,13 @@ uint16_t JYAxisInput;
 bool ButtonPressed;
 
 // The data transfer array
-byte JTransfer[2];
+int16_t TransArr[8];
 
-void InitMPU(int);
-
-void GetAngles(int, float[3], bool);
-void CorrectAngles(int, float[3]);
-void GetRawAccelerations(int);
-void GetRawRotations(int);
+void InitMPU(uint8_t);
+void GetAngles(uint8_t, int16_t[3], bool);
+void CorrectAngles(uint8_t, int16_t[3]);
+void GetRawAccelerations(uint8_t);
+void GetRawRotations(uint8_t);
 void PrintData();
 
 void setup() {
@@ -103,8 +102,8 @@ void setup() {
     Transmitter.setPALevel(RF24_PA_LOW);
     Transmitter.setChannel(88);
     // Setting transmission's properties
-    Transmitter.setPayloadSize(sizeof(JTransfer));
-    Transmitter.setAutoAck(true);
+    Transmitter.setPayloadSize(sizeof(TransArr));
+    Transmitter.setAutoAck(false);
     // Setting an amount and delay to retry failed transmissions
     Transmitter.setRetries(5, 3);
     // Setting the transmitter to send data
@@ -117,25 +116,33 @@ void setup() {
 void loop() {
     // Reading angles of the arm
     GetAngles(0, LowerAngles, true);
+
+    TransArr[0] = LowerAngles[0];
+    TransArr[1] = LowerAngles[1];
+    TransArr[2] = LowerAngles[2];
+
     GetAngles(1, UpperAngles, true);
 
-    // TODO: Packing them into the transfer array
+    TransArr[3] = LowerAngles[0];
+    TransArr[4] = LowerAngles[1];
+    TransArr[5] = LowerAngles[2];
 
     JYAxisInput = analogRead(J_PIN_Y);
     ButtonPressed = analogRead(J_PIN_B) <= 100;
 
-    JTransfer[0] = JYAxisInput;
-    JTransfer[1] = ButtonPressed;
+    TransArr[6] = JYAxisInput;
+    TransArr[7] = ButtonPressed;
 
     PrintData();
     // Writing one (first) byte to the radio channel
-    // if (Transmitter.write(&JTransfer, sizeof(JTransfer), 0)) {
-    // }
+    if (Transmitter.write(&TransArr, sizeof(TransArr) * 2)) {
+        Serial.println("Data sent");
+    }
 
     delay(TIMEOUT);
 }
 
-void InitMPU(int adr) {
+void InitMPU(uint8_t adr) {
     Wire.beginTransmission(adr);
     // Talk to the register 6B and reset it by writing a 0
     Wire.write(0x6B);
@@ -149,7 +156,7 @@ void InitMPU(int adr) {
 /// @param outArr - The output array to save calculations into.
 /// @param corrReq - The flag to determine,
 /// whether the angles should be automatically corrected or not.
-void GetAngles(int mpuNum, float (outArr)[3], bool corrReq) {
+void GetAngles(uint8_t mpuNum, int16_t (outArr)[3], bool corrReq) {
     // Reading MPU's raw data
     GetRawAccelerations(mpuNum);
     GetRawRotations(mpuNum);
@@ -163,12 +170,12 @@ void GetAngles(int mpuNum, float (outArr)[3], bool corrReq) {
     if (corrReq) {
         CorrectAngles(mpuNum, outArr);
     } else {
-        outArr[0] = AccAngs[mpuNum][0];
-        outArr[1] = AccAngs[mpuNum][1];
-        outArr[2] = AccAngs[mpuNum][2];
+        outArr[0] = (int16_t)round(AccAngs[mpuNum][0]);
+        outArr[1] = (int16_t)round(AccAngs[mpuNum][1]);
+        outArr[2] = (int16_t)round(AccAngs[mpuNum][2]);
     }
 }
-void CorrectAngles(int mpuNum, float outArr[3]) {
+void CorrectAngles(uint8_t mpuNum, int16_t outArr[3]) {
     // Adjusting acc/gyro influence
     AccMagnitude = sqrt(pow(AccAngs[mpuNum][0], 2) + pow(AccAngs[mpuNum][1], 2) + pow(AccAngs[mpuNum][2], 2));
     // * Note: the accelerator has higher influence
@@ -184,15 +191,15 @@ void CorrectAngles(int mpuNum, float outArr[3]) {
     GyroBiases[mpuNum][1] += Rots[mpuNum][1] * 0.0001;
     GyroBiases[mpuNum][2] += Rots[mpuNum][2] * 0.0001;
 
-    outArr[0] = Alphas[mpuNum] * (outArr[0] + (Rots[mpuNum][0] - GyroBiases[mpuNum][0]) * TIMEOUT / 1000)
-                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][0] + GyroCorrs[mpuNum][0]);
-    outArr[1] = Alphas[mpuNum] * (outArr[1] + (Rots[mpuNum][1] - GyroBiases[mpuNum][1]) * TIMEOUT / 1000)
-                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][1] + GyroCorrs[mpuNum][1]);
-    outArr[2] = Alphas[mpuNum] * (outArr[2] + (Rots[mpuNum][2] - GyroBiases[mpuNum][2]) * TIMEOUT / 1000)
-                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][2] + GyroCorrs[mpuNum][2]);
+    outArr[0] = (int16_t)round(Alphas[mpuNum] * (outArr[0] + (Rots[mpuNum][0] - GyroBiases[mpuNum][0]) * TIMEOUT / 1000)
+                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][0] + GyroCorrs[mpuNum][0]));
+    outArr[1] = (int16_t)round(Alphas[mpuNum] * (outArr[1] + (Rots[mpuNum][1] - GyroBiases[mpuNum][1]) * TIMEOUT / 1000)
+                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][1] + GyroCorrs[mpuNum][1]));
+    outArr[2] = (int16_t)round(Alphas[mpuNum] * (outArr[2] + (Rots[mpuNum][2] - GyroBiases[mpuNum][2]) * TIMEOUT / 1000)
+                 + (1 - Alphas[mpuNum]) * (AccAngs[mpuNum][2] + GyroCorrs[mpuNum][2]));
 }
 
-void GetRawAccelerations(int mpuNum) {
+void GetRawAccelerations(uint8_t mpuNum) {
     Wire.beginTransmission(Addresses[mpuNum]);
     Wire.write(0x3B);
     Wire.endTransmission(true);
@@ -204,7 +211,7 @@ void GetRawAccelerations(int mpuNum) {
     Accs[mpuNum][1] = (Wire.read() << 8 | Wire.read()) / 16384.0;
     Accs[mpuNum][2] = (Wire.read() << 8 | Wire.read()) / 16384.0;
 }
-void GetRawRotations(int mpuNum) {
+void GetRawRotations(uint8_t mpuNum) {
     Wire.beginTransmission(Addresses[mpuNum]);
     Wire.write(0x43);
     Wire.endTransmission(true);
--- ArmWriter/src/main.cpp
+++ ArmWriter/src/main.cpp
@@ -12,6 +12,18 @@ const byte R_ADDRESS[6] = "1RF24";
 
 byte JTransfer[2];
 
+const int AxisAmount = 3;
+// Arrays for ouput angles
+uint16_t UpperAngles[AxisAmount];  // The angles of upper arm
+uint16_t LowerAngles[AxisAmount];  // The angles of underarm
+
+uint16_t JYAxisInput;
+bool ButtonPressed;
+
+int16_t TransArr[3];
+
+void PrintData();
+
 void setup() {
     Serial.begin(9600);
     
@@ -26,8 +38,8 @@ void setup() {
     Receiver.setPALevel(RF24_PA_LOW);
     Receiver.setChannel(88);
     // Setting transmission's properties
-    Receiver.setPayloadSize(sizeof(JTransfer));
-    Receiver.setAutoAck(true);
+    Receiver.setPayloadSize(sizeof(TransArr));
+    Receiver.setAutoAck(false);
     // Setting the receiver to retrieve data
     Receiver.openReadingPipe(0, R_ADDRESS);
     Receiver.startListening();
@@ -37,13 +49,46 @@ void setup() {
 
 void loop() {
     if (Receiver.available() > 0) {
-        Receiver.read(&JTransfer, sizeof(JTransfer));
-        Serial.print("[+]-< JYAxis: ");
-        Serial.println(JTransfer[0]);
-        Serial.print("[+]-< JButton: ");
-        Serial.println(JTransfer[1]);
-        Serial.println(" |");
+        Receiver.read(&TransArr, sizeof(TransArr) * 2);
+
+        LowerAngles[0] = TransArr[0];
+        LowerAngles[1] = TransArr[1];
+        LowerAngles[2] = TransArr[2];
+
+        LowerAngles[3] = TransArr[3];
+        LowerAngles[4] = TransArr[4];
+        LowerAngles[5] = TransArr[5];
+
+        JYAxisInput = TransArr[6];
+        ButtonPressed = TransArr[7];
+
+        PrintData();
 
         delay(1000);
     }
+}
+
+void PrintData() {
+    Serial.print("[+]-< Upper MPU angles: ");
+    Serial.print("X: ");
+    Serial.print(UpperAngles[0]);
+    Serial.print(" Y: ");
+    Serial.print(UpperAngles[1]);
+    Serial.print(" Z: ");
+    Serial.println(UpperAngles[2]);
+
+    Serial.print("[+]-< Lower MPU angles: ");
+    Serial.print("X: ");
+    Serial.print(LowerAngles[0]);
+    Serial.print(" Y: ");
+    Serial.print(LowerAngles[1]);
+    Serial.print(" Z: ");
+    Serial.println(LowerAngles[2]);
+    Serial.println(" |");
+
+    Serial.print("[+]-< Joystick Y-axis input: ");
+    Serial.println(JYAxisInput);
+    Serial.print("[+]-< Joystick button signal: ");
+    Serial.println(ButtonPressed);
+    Serial.println(" |");
 }
